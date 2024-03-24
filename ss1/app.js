723715772b5c558a587ffdcf269a4baa1401bf83
quannhom <quannh12102009@gmail.com>
2024-03-24 17:31:15 +0700

    ss1 code

---
+++ ss1/app.js
@@ -0,0 +1,20 @@
+let student = [
+{
+    id: 1,
+    name: "Lê Anh",
+    age: 1,
+    major: "Computer Science",
+},
+{
+    id: 2,
+    name: "Vũ Dũng",
+    age: 2,
+    major: "Mathematics",
+},
+{
+    id: 3,
+    name: "Thùy Chi",   
+    age: 3,
+    major: "Calisthenic",
+}
+];
